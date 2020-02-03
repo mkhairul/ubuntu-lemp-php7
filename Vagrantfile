fdea8e337924a6dae9de0452a4bfd8647185ef68
Khairul Anuar <khairul.azizi@mediaprima.com.my>
2020-02-03 22:32:40 +0800

    init update, install mysql, create table, create user, install magento

--- Vagrantfile
+++ Vagrantfile
@@ -3,32 +3,30 @@
 
 Vagrant.configure(2) do |config|
 
-    config.vm.box = "bento/ubuntu-19.04"
-    config.vm.box_version = "201906.18.0"
+	config.vm.box = "bento/ubuntu-18.04"
+    #config.vm.box = "bento/ubuntu-19.10"
+    #config.vm.box_version = "201906.18.0"
     config.vm.box_check_update = true
     config.vbguest.auto_update = false
     config.vm.hostname = "lemp7"
     config.vm.define "lemp7"
 
-    config.vm.network :forwarded_port, guest: 80, host: 8082
-    config.vm.network :forwarded_port, guest: 3306, host: 3308
-    config.vm.network :forwarded_port, guest: 6379, host: 6381
-    config.vm.network :forwarded_port, guest: 27017, host: 27017
+    config.vm.network :forwarded_port, guest: 80, host: 8000
+    config.vm.network :forwarded_port, guest: 3306, host: 3306
+    #config.vm.network :forwarded_port, guest: 6379, host: 6381
+    #config.vm.network :forwarded_port, guest: 27017, host: 27017
 
     config.vm.network "private_network", ip: "192.168.2.2"
 
     config.vm.provider :virtualbox do |v|
-	v.name = "lemp7"
+		v.name = "lemp7"
         v.customize ["modifyvm", :id, "--memory", "2048"]
         v.customize ["modifyvm", :id, "--vram", "32"]
 	    v.customize ["modifyvm", :id, "--cpus", "2"]
     end
     
     # set project folder here:
-    config.vm.synced_folder "www/", "/var/www", 
-        owner: "www-data", 
-        group: "www-data",
-        mount_options: ["dmode=775,fmode=664"]
+    config.vm.synced_folder "C:/work", "/var/www"
 
-    config.vm.provision "shell", path: "./scripts/setup.sh"
+    config.vm.provision "shell", path: "./scripts/setup.sh", privileged: false
 end
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,6 +15,7 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install -y re2c libpcre3-dev gcc make
+sudo apt-get install wget
 
 echo -e "----------------------------------------"
 echo "VAGRANT ==> tools (mc, htop, unzip etc...)"
@@ -31,8 +32,8 @@ sudo apt-get install -y nginx
 # php
 #
 echo -e "----------------------------------------"
-echo "VAGRANT ==> PHP 7"
-sudo apt-get install -y php7.3-fpm php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-mysql php7.3-phpdbg php7.3-mbstring php7.3-gd php-imagick  php7.3-pgsql php7.3-pspell php7.3-recode php7.3-tidy php7.3-dev php7.3-intl php7.3-gd php7.3-curl php7.3-zip php7.3-xml php-memcached mcrypt memcached phpunit
+echo "VAGRANT ==> PHP 7.3"
+sudo apt-get install -y php7.3-fpm php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-mysql php7.3-phpdbg php7.3-mbstring php7.3-gd php-imagick  php7.3-pgsql php7.3-pspell php7.3-recode php7.3-tidy php7.3-dev php7.3-intl php7.3-gd php7.3-curl php7.3-zip php7.3-xml php-memcached mcrypt memcached phpunit php7.3-bcmath php7.3-soap php7.3-xsl libxml2-dev
 sudo apt-get install -y php7.3-dev
 
 #
@@ -59,8 +60,66 @@ sudo mv composer.phar /usr/local/bin/composer
 # Frontend Tools (npm, nodejs, gulp)
 #
 echo -e "----------------------------------------"
-echo "VAGRANT ==> Frontend Tools (npm, nodejs)"
-sudo apt install -y nodejs npm
+echo "VAGRANT ==> Frontend Tools (nvm, npm, nodejs)"
+git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
+echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
+source /home/vagrant/.profile
+echo "VAGRANT ==> Installing node.js... (please be patient)"
+nvm install stable &> /dev/null
+nvm alias default stable
+
+#
+# MySQL Server
+#
+echo -e "----------------------------------------"
+echo "VAGRANT ==> MySQL"
+sudo apt-get install -y mysql-server
+#sudo sed '/\[mysqld\]/askip-grant-tables' -i /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo mysql -u root -e "CREATE DATABASE 'magento'; CREATE USER 'magento'@'localhost' IDENTIFIED BY 'magento'; GRANT ALL PRIVILEGES ON magento.* TO 'magento'@'localhost'; FLUSH PRIVILEGES"
+
+
+#
+# Magento
+#
+echo -e "----------------------------------------"
+echo "VAGRANT ==> Magento"
+sudo bash -c 'cat << EOF > /etc/nginx/sites-available/magento
+upstream fastcgi_backend {
+  server  unix:/run/php/php7.3-fpm.sock;
+}
+
+server {
+
+  listen 80;
+  server_name _;
+  set $MAGE_ROOT /home/vagrant/magento;
+  include /home/vagrant/magento/nginx.conf.sample;
+}
+EOF'
+git clone https://github.com/magento/magento2.git /home/vagrant/magento
+cd /home/vagrant/magento
+git checkout 2.3.4
+find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
+find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
+chown -R :www-data .
+chmod u+x bin/magento
+composer install
+bin/magento setup:install \
+--base-url=http://localhost \
+--db-host=localhost \
+--db-name=magento \
+--db-user=magento \
+--db-password=magento \
+--backend-frontname=admin \
+--admin-firstname=admin \
+--admin-lastname=admin \
+--admin-email=admin@admin.com \
+--admin-user=admin \
+--admin-password=admin123 \
+--language=en_US \
+--currency=MYR \
+--timezone=Asia/Kuala_Lumpur \
+--use-rewrites=1
 
 #
 # Reload servers
